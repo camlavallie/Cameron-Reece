27c3cd640cde5245a99bc9bb43affa0942900350
Cameron LaVallie <camlavallie@gmail.com>
2020-05-28 12:07:32 -0500

    summit

--- pages/summit/index.vue
+++ pages/summit/index.vue
@@ -1,58 +1,94 @@
 <template>
   <!-- component -->
-<main class="py-4">
-  <div class="px-4">
-    <div class="block md:flex justify-between md:-mx-2">
-      <div class="w-full lg:w-1/3 md:mx-2 mb-4 md:mb-0">
-        <div class="bg-white my-24 m-4 rounded-lg overflow-hidden  relative">
-          <img class="object-cover object-center" src="../../assets/summit-images/SWS.png" alt="">
+  <main class="py-4">
+    <div class="px-4">
+      <div class="block md:flex justify-between md:-mx-2">
+        <div class="w-full lg:w-1/3 md:mx-2 mb-4 md:mb-0">
+          <div class="bg-white my-24 m-4 rounded-lg overflow-hidden relative">
+            <img class="object-cover object-center" src="../../assets/summit-images/SWS.png" alt>
+          </div>
         </div>
-      </div>
-      <div class="w-full lg:w-1/3 md:mx-2 mb-4 md:mb-0">
-        <div class="bg-white my-24 m-4 rounded-lg overflow-hidden  relative">
-          <img class="object-cover object-center" src="../../assets/summit-images/SWS2.png" alt="">
+        <div class="w-full lg:w-1/3 md:mx-2 mb-4 md:mb-0">
+          <div class="bg-white my-24 m-4 rounded-lg overflow-hidden relative">
+            <img class="object-cover object-center" src="../../assets/summit-images/SWS2.png" alt>
+          </div>
         </div>
-      </div>
-      <div class="w-full lg:w-1/3 md:mx-2 mb-4 md:mb-0">
-        <div class="bg-white my-24 m-4 rounded-lg overflow-hidden  relative">
-          <img class="object-cover object-center" src="../../assets/summit-images/SWS4.png" alt="">
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <div class="px-4">
-    <div class="block md:flex justify-between md:-mx-2">
-      <div class="w-full lg:w-1/3 md:mx-2 mb-4 md:mb-0">
-        <div class="bg-white my-24 m-4 rounded-lg overflow-hidden  relative">
-          <img class="object-cover object-center" src="../../assets/summit-images/SWS10.png" alt="">
-        </div>
-      </div>
-      <div class="w-full lg:w-1/3 md:mx-2 mb-4 md:mb-0">
-        <div class="bg-white my-24 m-4 rounded-lg overflow-hidden  relative">
-          <img class="object-cover object-center" src="../../assets/summit-images/SWS6.png" alt="">
-        </div>
-      </div>
-      <div class="w-full lg:w-1/3 md:mx-2 mb-4 md:mb-0">
-        <div class="bg-white my-24 m-4 rounded-lg overflow-hidden  relative">
-          <img class="object-cover object-center" src="../../assets/summit-images/SWS7.png" alt="">
+        <div class="w-full lg:w-1/3 md:mx-2 mb-4 md:mb-0">
+          <div class="bg-white my-24 m-4 rounded-lg overflow-hidden relative">
+            <img class="object-cover object-center" src="../../assets/summit-images/SWS4.png" alt>
+          </div>
         </div>
       </div>
     </div>
-  </div>
 
-  <div class="px-4">
-    <div class="block md:flex justify-between md:-mx-2">
-      <div class="w-full lg:w-1/3 md:mx-2 mb-4 md:mb-0">
-        <div class="bg-white my-24 m-4 rounded-lg overflow-hidden  relative">
-          <img class="object-cover object-center" src="../../assets/summit-images/SWS8.png" alt="">
+    <div class="px-4">
+      <div class="block md:flex justify-between md:-mx-2">
+        <div class="w-full lg:w-1/3 md:mx-2 mb-4 md:mb-0">
+          <div class="bg-white my-24 m-4 rounded-lg overflow-hidden relative">
+            <img class="object-cover object-center" src="../../assets/summit-images/SWS10.png" alt>
+          </div>
+        </div>
+        <div class="w-full lg:w-1/3 md:mx-2 mb-4 md:mb-0">
+          <div class="bg-white my-24 m-4 rounded-lg overflow-hidden relative">
+            <img class="object-cover object-center" src="../../assets/summit-images/SWS6.png" alt>
+          </div>
+        </div>
+        <div class="w-full lg:w-1/3 md:mx-2 mb-4 md:mb-0">
+          <div class="bg-white my-24 m-4 rounded-lg overflow-hidden relative">
+            <img class="object-cover object-center" src="../../assets/summit-images/SWS7.png" alt>
+          </div>
         </div>
       </div>
-    
     </div>
-  </div>
 
+    <div class="px-4">
+      <div class="block md:flex justify-between md:-mx-2">
+        <div class="w-full lg:w-1/3 md:mx-2 mb-4 md:mb-0">
+          <div class="bg-white my-24 m-4 rounded-lg overflow-hidden relative">
+            <img class="object-cover object-center" src="../../assets/summit-images/SWS8.png" alt>
+          </div>
+        </div>
+        <div class="w-full lg:w-1/3 md:mx-2 mb-4 md:mb-0">
+          <div class="bg-white my-24 m-4 rounded-lg overflow-hidden relative">
+            <img class="object-cover object-center" src="../../assets/summit-images/SWS11.png" alt>
+          </div>
+        </div>
+        <div class="w-full lg:w-1/3 md:mx-2 mb-4 md:mb-0">
+          <div class="bg-white my-24 m-4 rounded-lg overflow-hidden relative">
+            <img class="object-cover object-center" src="../../assets/summit-images/SWS12.png" alt>
+          </div>
+        </div>
+      </div>
+    </div>
 
+    <div class="px-4">
+      <div class="block md:flex justify-between md:-mx-2">
+        <div class="w-full lg:w-1/3 md:mx-2 mb-4 md:mb-0">
+          <div class="bg-white my-24 m-4 rounded-lg overflow-hidden relative">
+            <img class="object-cover object-center" src="../../assets/summit-images/SWS13.png" alt>
+          </div>
+        </div>
+        <div class="w-full lg:w-1/3 md:mx-2 mb-4 md:mb-0">
+          <div class="bg-white my-24 m-4 rounded-lg overflow-hidden relative">
+            <img class="object-cover object-center" src="../../assets/summit-images/SWS14.png" alt>
+          </div>
+        </div>
+        <div class="w-full lg:w-1/3 md:mx-2 mb-4 md:mb-0">
+          <div class="bg-white my-24 m-4 rounded-lg overflow-hidden relative">
+            <img class="object-cover object-center" src="../../assets/summit-images/SWS15.png" alt>
+          </div>
+        </div>
+      </div>
+    </div>
 
-</main>
+    <div class="px-4">
+      <div class="block md:flex justify-between md:-mx-2">
+        <div class="w-full lg:w-1/3 md:mx-2 mb-4 md:mb-0">
+          <div class="bg-white my-24 m-4 rounded-lg overflow-hidden relative">
+            <img class="object-cover object-center" src="../../assets/summit-images/SWS16.png" alt>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
