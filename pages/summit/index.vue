57f9a2e8277e1480cc9e3342cfbc1c397a3d8d22
Cameron LaVallie <camlavallie@gmail.com>
2020-06-08 11:16:49 -0500

    SEO

--- pages/summit/index.vue
+++ pages/summit/index.vue
@@ -78,5 +78,17 @@
         >
       </div>
     </div>
+    
+    <div
+      class="bg-cover bg-center h-auto text-white py-24 px-10 object-fill my-24 bg-gray-900"
+    >
+      <div class="md:w-1/2">
+        <img
+          class="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 object-cover object-center"
+          src="../../assets/summit-images/Mount-10.png"
+          alt
+        >
+      </div>
+    </div>
   </main>
 </template>
