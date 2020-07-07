0744c9e88f3fb6a48d761eb101c92f3cbb04ce38
Cameron LaVallie <camlavallie@gmail.com>
2020-07-07 12:29:11 -0500

    summit

--- pages/summit/index.vue
+++ pages/summit/index.vue
@@ -3,39 +3,34 @@
 
   <main class>
     <!-- component -->
-    <div>
-    <div
-      class="bg-cover bg-center h-auto text-white py-24 px-10 object-fill my-24 bg-gray-900"
-    >
-      <div class="md:w-8/12">
-        <img
-          class="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 object-cover object-center"
-          src="../../assets/summit-images/Mount-1.png"
-          alt
-        >
-      </div>
-    </div>
-    </div>
-    <div
-      class="bg-cover bg-center h-auto text-white py-24 px-10 object-fill my-24 bg-gray-900"
-    >
-      <div class="md:w-7/12">
-        <img
-          class="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 object-cover object-center"
-          src="../../assets/summit-images/Mount-2.png"
-          alt
-        >
-      </div>
-    </div>
+  
+ 
+    
+  
+    
+ 
+    
     
     
     <div
-      class="bg-cover bg-center h-auto text-white py-24 px-10 object-fill my-24 bg-gray-900"
+      class="bg-cover bg-center h-auto text-white py-24 px-10 object-fill my-24 "
     >
       <div class="md:w-1/2">
         <img
           class="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 object-cover object-center"
-          src="../../assets/summit-images/Mount-8.png"
+          src="../../assets/summit-images/SummitWealthMaster-blue.png"
+          alt
+        >
+      </div>
+    </div>
+    
+    <div
+      class="bg-cover bg-center h-auto text-white py-24 px-10 object-fill my-24"
+    >
+      <div class="md:w-1/2">
+        <img
+          class="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 object-cover object-center"
+          src="../../assets/summit-images/SummitWealthMaster-2.png"
           alt
         >
       </div>
@@ -47,33 +42,7 @@
       <div class="md:w-1/2">
         <img
           class="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 object-cover object-center"
-          src="../../assets/summit-images/Mount-4.png"
-          alt
-        >
-      </div>
-    </div>
-    
-    
-    
-    <div
-      class="bg-cover bg-center h-auto text-white py-24 px-10 object-fill my-24 bg-gray-900"
-    >
-      <div class="md:w-1/2">
-        <img
-          class="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 object-cover object-center"
-          src="../../assets/summit-images/Mount-7.png"
-          alt
-        >
-      </div>
-    </div>
-    
-    <div
-      class="bg-cover bg-center h-auto text-white py-24 px-10 object-fill my-24 bg-gray-900"
-    >
-      <div class="md:w-1/2">
-        <img
-          class="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 object-cover object-center"
-          src="../../assets/summit-images/Mount-9.png"
+          src="../../assets/summit-images/SummitWealthMaster-5.png"
           alt
         >
       </div>
