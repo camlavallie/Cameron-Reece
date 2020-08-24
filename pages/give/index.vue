74d3e2255460b38d6e5e8399ac3e31e55a10d35d
Cameron LaVallie <camlavallie@gmail.com>
2020-08-24 14:39:29 -0500

    message 2

--- pages/give/index.vue
+++ pages/give/index.vue
@@ -1,56 +1,111 @@
 <template>
   <div>
-<div class="text-white mt-0 border border-blue-800 py-0 w-full h-full" style="background-image:url('https://images.pexels.com/photos/32237/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=750&w=1260')">
-    <header class="flex md:justify-between p-8">    
-    </header>
-    <section class="px-2 lg:px-0 mt-16 sm:mt-32 flex w-full container mx-auto pb-16">
-        <div class="w-full text-white flex justify-center md:justify-start text-center md:text-left">
-            <div class="md:w-1/2 w-full">
-                <h2 class="leading-none font-bold text-2xl xs:text-2x1 md:text-5xl lg:6x1 uppercase">Give & <span class="text-blue-400">Pray</span></h2>
-                <p class="mt-12 mb-12">Thank you for stopping by! If you'd like to support me during my time at Lifestye Christianity University this coming fall and spring please click the donate button below!</p>
-                <a href="https://students.lifestylechristianityu.com/donations/transactions/new?student_binder=4227" target="_blank" class="px-16 rounded-full bg-blue-400 text-white font-bold p-4 uppercase border-blue-500 border">Donate!</a>
-            </div>
-            <div class="md:w-1/2 md:justify-center md:flex hidden">
-                <img src="https://images.pexels.com/photos/2014773/pexels-photo-2014773.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="rounded-lg border border-white" alt="worship" />
-            </div>
+    <div
+      class="text-white mt-0 border border-blue-800 py-0 w-full h-full"
+      style="background-image:url('https://images.pexels.com/photos/32237/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=750&w=1260')"
+    >
+      <header class="flex md:justify-between p-8"></header>
+      <section class="px-2 lg:px-0 mt-16 sm:mt-32 flex w-full container mx-auto pb-16">
+        <div
+          class="w-full text-white flex justify-center md:justify-start text-center md:text-left"
+        >
+          <div class="md:w-1/2 w-full">
+            <h2 class="leading-none font-bold text-2xl xs:text-2x1 md:text-5xl lg:6x1 uppercase">
+              Give &
+              <span class="text-blue-400">Pray</span>
+            </h2>
+            <p
+              class="mt-12 mb-12"
+            >Thank you for stopping by! If you'd like to support me during my time at Lifestye Christianity University this coming fall and spring please click the donate button below!</p>
+            <a
+              href="https://lcu.populiweb.com/router/studentcharges/sh
+ow/gc2b24e69c08a966d31f05300b9617d3669875407b90ee3
+0a10fcb64850158a4340aac4c41c6dda658226e9af66a1f183
+1441240939e858c3de88b278b09d1590a6502818a5f7308b3a
+d1"
+              target="_blank"
+              class="px-16 rounded-full bg-blue-400 text-white font-bold p-4 uppercase border-blue-500 border"
+            >Donate!</a>
+          </div>
+          <div class="md:w-1/2 md:justify-center md:flex hidden">
+            <img
+              src="https://images.pexels.com/photos/2014773/pexels-photo-2014773.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+              class="rounded-lg border border-white"
+              alt="worship"
+            >
+          </div>
         </div>
-    </section>
-    <section class="bg-gray-900 sm:py-32 py-16">
-        <div class="container mx-auto sm:flex-row flex-col flex ">
-       
-            <div class="sm:w-1/3 w-full sm:mb-0 mb-16">
-                <img src="https://images.pexels.com/photos/1022922/pexels-photo-1022922.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="rounded-full w-24 h-24 mx-auto mb-2">
-                <p class="italic text-gray-500 text-center my-3">This is a year journey that I know will make an impact on my life, my family and all those I come into contact with.</p>
-                <p class="font-bold  text-gray-400 text-center text-xl">Seeking First | His Kingdom</p>
-            </div>
-               <div class="sm:w-1/3 w-full sm:mb-0 mb-16">
-                <img src="https://images.pexels.com/photos/269633/pexels-photo-269633.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="rounded-full w-24 h-24 mx-auto mb-2">
-                <p class="italic text-gray-500 text-center my-3">Jesus is center piece in heaven. <br/> He should be center piece here.</p>
-                <p class="font-bold  text-gray-400 text-center text-xl"> Michael Koulianos</p>
-            </div>
+      </section>
+      <section class="bg-gray-900 sm:py-32 py-16">
+        <div class="container mx-auto sm:flex-row flex-col flex">
+          <div class="sm:w-1/3 w-full sm:mb-0 mb-16">
+            <img
+              src="https://images.pexels.com/photos/1022922/pexels-photo-1022922.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+              class="rounded-full w-24 h-24 mx-auto mb-2"
+            >
+            <p
+              class="italic text-gray-500 text-center my-3"
+            >This is a year journey that I know will make an impact on my life, my family and all those I come into contact with.</p>
+            <p class="font-bold text-gray-400 text-center text-xl">Seeking First | His Kingdom</p>
+          </div>
+          <div class="sm:w-1/3 w-full sm:mb-0 mb-16">
+            <img
+              src="https://images.pexels.com/photos/269633/pexels-photo-269633.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+              class="rounded-full w-24 h-24 mx-auto mb-2"
+            >
+            <p class="italic text-gray-500 text-center my-3">
+              Jesus is center piece in heaven.
+              <br>He should be center piece here.
+            </p>
+            <p class="font-bold text-gray-400 text-center text-xl">Michael Koulianos</p>
+          </div>
 
-            <div class="sm:w-1/3 w-full sm:mb-0 mb-16">
-                <img src="https://images.pexels.com/photos/2685175/pexels-photo-2685175.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="rounded-full w-24 h-24 mx-auto mb-2">
-                <p class="italic text-gray-500 text-center my-3">Jesus said to His disciples, “If anyone wishes to come after Me, he must deny himself, and take up his cross and follow Me."</p>
-                <p class="font-bold  text-gray-400 text-center text-xl">Matthew 16:24</p>
-            </div>
-            
+          <div class="sm:w-1/3 w-full sm:mb-0 mb-16">
+            <img
+              src="https://images.pexels.com/photos/2685175/pexels-photo-2685175.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+              class="rounded-full w-24 h-24 mx-auto mb-2"
+            >
+            <p
+              class="italic text-gray-500 text-center my-3"
+            >Jesus said to His disciples, “If anyone wishes to come after Me, he must deny himself, and take up his cross and follow Me."</p>
+            <p class="font-bold text-gray-400 text-center text-xl">Matthew 16:24</p>
+          </div>
         </div>
-    </section>
-    <section class="bg-gray-800 py-32">
-            <h1 class="text-5xl font-bold uppercase text-center text-gray-300">Want to know more?</h1>
+      </section>
+      <section class="bg-gray-800 py-32">
+        <h1 class="text-5xl font-bold uppercase text-center text-gray-300">Want to know more?</h1>
         <div class="container flex flex-wrap justify-center mx-auto">
-            
-                
-<iframe class="m-3" width="560" height="315" src="https://www.youtube.com/embed/j1aXXCl3b_M" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <iframe
+            class="m-3"
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/j1aXXCl3b_M"
+            frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
 
-          <iframe class="m-3" width="560" height="315" src="https://www.youtube.com/embed/1x77T0bIBaA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                
-<iframe class="m-3" width="560" height="315" src="https://www.youtube.com/embed/-u6YE5VC7Rg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <iframe
+            class="m-3"
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/1x77T0bIBaA"
+            frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
 
-    
+          <iframe
+            class="m-3"
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/-u6YE5VC7Rg"
+            frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
         </div>
-    </section>
-</div>
+      </section>
+    </div>
   </div>
 </template>
