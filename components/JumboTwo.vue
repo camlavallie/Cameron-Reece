31e1aff9c0ebcfec2184bdc363710b09af123439
Cameron LaVallie <camlavallie@gmail.com>
2021-03-24 08:49:23 -0500

    recent work

--- components/JumboTwo.vue
+++ components/JumboTwo.vue
@@ -27,6 +27,29 @@
     </div>
     <div class="md:w-1/2 lg:w-1/3 py-4 px-4">
       <div class>
+        <a href="https://www.kingdom-handyman.com/" target="_blank">
+          <div class="bg-white relative shadow p-2 rounded-lg text-gray-800 hover:shadow-lg">
+            <div
+              class="right-0 mt-4 rounded-l-full absolute text-center font-bold text-xs text-white px-2 py-1 bg-blue-500"
+            >Visit the site!</div>
+            <img src="../assets/images/khm.png" class="h-32 rounded-lg w-full object-cover">
+            <div class="flex justify-center">
+              <img
+                src="../assets/images/nuxt-icon.png"
+                class="rounded-full -mt-6 border-4 object-center object-cover border-white mr-2 h-16 w-16"
+              >
+            </div>
+            <div class="py-2 px-2">
+              <div class="font-bold font-title text-center">Kingdom Handyman Services</div>
+
+              <div class="text-sm font-light text-center my-2">Created with Nuxt/Vue</div>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+    <div class="md:w-1/2 lg:w-1/3 py-4 px-4">
+      <div class>
         <a href="https://www.legacyteams.net/" target="_blank">
           <div class="bg-white relative shadow p-2 rounded-lg text-gray-800 hover:shadow-lg">
             <div
