3f13e3c7ac1b05b9fd3b28c1e59d5b28353802cb
Cameron LaVallie <camlavallie@gmail.com>
2021-07-04 16:51:59 -0500

    edits

--- components/JumboThree.vue
+++ components/JumboThree.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="rounded bg-white text-center px-6 py-4">
       <div id="wrapper" class="max-w-xl px-4 py-4 mx-auto">
-        <div class="sm:grid sm:h-32 sm:grid-flow-row sm:gap-4 sm:grid-cols-3">
+        <div class="flex flex-col justify-center m-3">
           <div
             id="jh-stats-neutral"
             class="flex flex-col justify-center px-4 py-4 mt-4 bg-white border border-gray-300 rounded sm:mt-0"
@@ -23,7 +23,10 @@
                 </p>
               </div>
               <div class="flex justify-center">
-                <img src="../assets/images/nuxt.svg" class="-mt-6 object-center h-16 w-16">
+                <img
+                  src="../assets/images/nuxt.svg"
+                  class="-mt-6 object-center h-16 w-16"
+                />
               </div>
               <p class="text-lg text-center text-gray-500">NuxtJS</p>
             </div>
@@ -49,7 +52,10 @@
                 </p>
               </div>
               <div class="flex justify-center">
-                <img src="../assets/images/vue.svg" class="-mt-6 object-center h-12 w-12">
+                <img
+                  src="../assets/images/vue.svg"
+                  class="-mt-6 object-center h-12 w-12"
+                />
               </div>
               <p class="text-lg text-center text-gray-500">VueJS</p>
             </div>
@@ -77,9 +83,38 @@
                 <img
                   src="../assets/images/react.svg"
                   class="rounded-full -mt-6 object-center h-16 w-16"
-                >
+                />
               </div>
               <p class="text-lg text-center text-gray-500">ReactJS</p>
+            </div>
+          </div>
+
+          <div
+            id="jh-stats-neutral"
+            class="flex flex-col justify-center px-4 py-4 mt-4 bg-white border border-gray-300 rounded sm:mt-0"
+          >
+            <div>
+              <div>
+                <p class="flex items-center justify-end text-green-500 text-md">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="w-5 h-5 fill-current"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      class="heroicon-ui"
+                      d="M20 15a1 1 0 002 0V7a1 1 0 00-1-1h-8a1 1 0 000 2h5.59L13 13.59l-3.3-3.3a1 1 0 00-1.4 0l-6 6a1 1 0 001.4 1.42L9 12.4l3.3 3.3a1 1 0 001.4 0L20 9.4V15z"
+                    ></path>
+                  </svg>
+                </p>
+              </div>
+              <div class="flex justify-center">
+                <img
+                  src="../assets/images/nextjs-icon.svg"
+                  class="rounded-full -mt-6 border-4 object-center object-cover border-white h-16 w-16"
+                />
+              </div>
+              <p class="text-lg text-center text-gray-500">NextJS</p>
             </div>
           </div>
 
@@ -103,7 +138,10 @@
                 </p>
               </div>
               <div class="flex justify-center">
-                <img src="../assets/images/angular-icon.svg" class="-mt-6 object-center h-16 w-16">
+                <img
+                  src="../assets/images/angular-icon.svg"
+                  class="-mt-6 object-center h-16 w-16"
+                />
               </div>
               <p class="text-lg text-center text-gray-500">AngularJS</p>
             </div>
@@ -130,11 +168,127 @@
               </div>
               <div class="flex justify-center">
                 <img
+                  src="../assets/images/apollostack.svg"
+                  class="rounded-full -mt-6 border-4 object-center object-cover border-white h-16 w-16"
+                />
+              </div>
+              <p class="text-lg text-center text-gray-500">Apollo</p>
+            </div>
+          </div>
+
+          <div
+            id="jh-stats-neutral"
+            class="flex flex-col justify-center px-4 py-4 mt-4 bg-white border border-gray-300 rounded sm:mt-0"
+          >
+            <div>
+              <div>
+                <p class="flex items-center justify-end text-green-500 text-md">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="w-5 h-5 fill-current"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      class="heroicon-ui"
+                      d="M20 15a1 1 0 002 0V7a1 1 0 00-1-1h-8a1 1 0 000 2h5.59L13 13.59l-3.3-3.3a1 1 0 00-1.4 0l-6 6a1 1 0 001.4 1.42L9 12.4l3.3 3.3a1 1 0 001.4 0L20 9.4V15z"
+                    ></path>
+                  </svg>
+                </p>
+              </div>
+              <div class="flex justify-center">
+                <img
+                  src="../assets/images/keystonejs.svg"
+                  class="rounded-full -mt-6 border-4 object-center object-cover border-white h-16 w-16"
+                />
+              </div>
+              <p class="text-lg text-center text-gray-500">KeystoneJS</p>
+            </div>
+          </div>
+
+          <div
+            id="jh-stats-neutral"
+            class="flex flex-col justify-center px-4 py-4 mt-4 bg-white border border-gray-300 rounded sm:mt-0"
+          >
+            <div>
+              <div>
+                <p class="flex items-center justify-end text-green-500 text-md">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="w-5 h-5 fill-current"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      class="heroicon-ui"
+                      d="M20 15a1 1 0 002 0V7a1 1 0 00-1-1h-8a1 1 0 000 2h5.59L13 13.59l-3.3-3.3a1 1 0 00-1.4 0l-6 6a1 1 0 001.4 1.42L9 12.4l3.3 3.3a1 1 0 001.4 0L20 9.4V15z"
+                    ></path>
+                  </svg>
+                </p>
+              </div>
+              <div class="flex justify-center">
+                <img
+                  src="../assets/images/graphql.svg"
+                  class="rounded-full -mt-6 border-4 object-center object-cover border-white h-16 w-16"
+                />
+              </div>
+              <p class="text-lg text-center text-gray-500">GraphQL</p>
+            </div>
+          </div>
+
+          <div
+            id="jh-stats-neutral"
+            class="flex flex-col justify-center px-4 py-4 mt-4 bg-white border border-gray-300 rounded sm:mt-0"
+          >
+            <div>
+              <div>
+                <p class="flex items-center justify-end text-green-500 text-md">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="w-5 h-5 fill-current"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      class="heroicon-ui"
+                      d="M20 15a1 1 0 002 0V7a1 1 0 00-1-1h-8a1 1 0 000 2h5.59L13 13.59l-3.3-3.3a1 1 0 00-1.4 0l-6 6a1 1 0 001.4 1.42L9 12.4l3.3 3.3a1 1 0 001.4 0L20 9.4V15z"
+                    ></path>
+                  </svg>
+                </p>
+              </div>
+              <div class="flex justify-center">
+                <img
                   src="../assets/images/tailwind.jpg"
                   class="rounded-full -mt-6 border-4 object-center object-cover border-white h-16 w-16"
-                >
+                />
               </div>
               <p class="text-lg text-center text-gray-500">TailwindCSS</p>
+            </div>
+          </div>
+
+          <div
+            id="jh-stats-neutral"
+            class="flex flex-col justify-center px-4 py-4 mt-4 bg-white border border-gray-300 rounded sm:mt-0"
+          >
+            <div>
+              <div>
+                <p class="flex items-center justify-end text-green-500 text-md">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="w-5 h-5 fill-current"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      class="heroicon-ui"
+                      d="M20 15a1 1 0 002 0V7a1 1 0 00-1-1h-8a1 1 0 000 2h5.59L13 13.59l-3.3-3.3a1 1 0 00-1.4 0l-6 6a1 1 0 001.4 1.42L9 12.4l3.3 3.3a1 1 0 001.4 0L20 9.4V15z"
+                    ></path>
+                  </svg>
+                </p>
+              </div>
+              <div class="flex justify-center">
+                <img
+                  src="../assets/images/styledcomp.png"
+                  class="rounded-full -mt-6 border-4 object-center object-cover border-white h-12 w-12"
+                />
+              </div>
+              <p class="text-lg text-center text-gray-500">Styled Components</p>
             </div>
           </div>
 
@@ -158,7 +312,10 @@
                 </p>
               </div>
               <div class="flex justify-center">
-                <img src="../assets/images/bootstrap.svg" class="-mt-6 object-center h-16 w-16">
+                <img
+                  src="../assets/images/bootstrap.svg"
+                  class="-mt-6 object-center h-16 w-16"
+                />
               </div>
               <p class="text-lg text-center text-gray-500">Bootstrap</p>
             </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,64 @@
 <template>
   <div class="container">
     <div>
-      <JumboOne/>
+      <JumboOne />
 
       <div class="my-24" id="work">
         <h1 class="text-3xl lg:text-5xl font-thin">Recent Work</h1>
-        <JumboTwo/>
+        <JumboTwo />
       </div>
       <div class="inset-x-0 bottom-0 relative trasnparent-25">
-        <JumboButtons/>
+        <JumboButtons />
       </div>
 
-      <div class="mb-24" id="skills">
+      <div class="my-24" id="skills">
         <h1 class="text-3xl lg:text-5xl font-thin">My Favorite Things!</h1>
-        <JumboThree/>
+        <JumboThree />
+        <div class="mb-4">
+          <svg
+            class="inline-block fill-current text-grey h-24 w-24"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+          >
+            <path
+              d="M11.933 13.069s7.059-5.094 6.276-10.924a.465.465 0 0 0-.112-.268.436.436 0 0 0-.263-.115C12.137.961 7.16 8.184 7.16 8.184c-4.318-.517-4.004.344-5.974 5.076-.377.902.234 1.213.904.959l2.148-.811 2.59 2.648-.793 2.199c-.248.686.055 1.311.938.926 4.624-2.016 5.466-1.694 4.96-6.112zm1.009-5.916a1.594 1.594 0 0 1 0-2.217 1.509 1.509 0 0 1 2.166 0 1.594 1.594 0 0 1 0 2.217 1.509 1.509 0 0 1-2.166 0z"
+              class="text-gray-900"
+            ></path>
+          </svg>
+        </div>
+        <p class="text-2xl text-grey-900 hover:border-b-2 font-medium mb-4">
+          Skies The Limit
+        </p>
+        <div>
+          <a
+            href="https://docs.google.com/document/d/1bMCzmpW2__NSs59VDNH47VQ5mH7soQ0UVL50XLG7WfY/edit?usp=sharing"
+            target="_blank"
+          >
+            <button
+              class="bg-gray-900 hover:bg-white border-b-2 hover:border-gray-900 hover:text-gray-900 text-white font-bold py-2 px-4 rounded-lg mb-8"
+            >
+              Resume 🚀
+            </button>
+            <a
+              href="/contact"
+              class="bg-gray-900 hover:bg-white border-b-2 hover:border-gray-900 hover:text-gray-900 text-white font-bold py-2 px-4 rounded-lg mb-8"
+              >Contact Me ✏️</a
+            >
+          </a>
+        </div>
       </div>
-
-      <div>
-        <JumboFour/>
-      </div>
-      <div class="mt-12 mb-24">
-        <JumboFive/>
-      </div>
+      <div></div>
     </div>
   </div>
 </template>
 
 <script>
-import JumboOne from "~/components/JumboOne.vue";
-import JumboTwo from "~/components/JumboTwo.vue";
-import JumboThree from "~/components/JumboThree.vue";
-import JumboFour from "~/components/JumboFour.vue";
-import JumboFive from "~/components/JumboFive.vue";
-import JumboButtons from "~/components/JumboButtons.vue";
+import JumboOne from '~/components/JumboOne.vue'
+import JumboTwo from '~/components/JumboTwo.vue'
+import JumboThree from '~/components/JumboThree.vue'
+import JumboFour from '~/components/JumboFour.vue'
+import JumboFive from '~/components/JumboFive.vue'
+import JumboButtons from '~/components/JumboButtons.vue'
 
 export default {
   components: {
@@ -44,24 +70,24 @@ export default {
     JumboButtons
   },
   head: {
-    title: "Cameron Reece",
+    title: 'Cameron Reece',
     meta: [
       {
-        charset: "utf-8"
+        charset: 'utf-8'
       },
       {
-        name: "viewport",
-        content: "width=device-width, initial-scale=1"
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
       },
       {
-        hid: "description",
-        name: "description",
+        hid: 'description',
+        name: 'description',
         content:
-          "My Name is Cameron Reece LaVallie and Im a Junior Software Developer, a Bethel School of Technology (Rated #1 in Bootcamps) Graduate. Located in Fort Worth, Texas. React, Angular, Tailwind, Vue, and Nuxt are my jam."
+          'My Name is Cameron Reece LaVallie and Im a Junior Software Developer, a Bethel School of Technology (Rated #1 in Bootcamps) Graduate. Located in Fort Worth, Texas. React, Angular, Tailwind, Vue, and Nuxt are my jam.'
       }
     ]
   }
-};
+}
 </script>
 
 <style>
@@ -87,8 +113,8 @@ html {
 }
 
 .title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
   font-weight: 300;
   color: #35495e;
