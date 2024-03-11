dd5ae216c957d3bae09463c8d6c97c6c35995271
Cameron LaVallie <camlavallie@gmail.com>
2024-03-11 16:25:17 -0500

    fixing bug

--- components/JumboOne.vue
+++ components/JumboOne.vue
@@ -4,16 +4,21 @@
       <img
         src="https://images.pexels.com/photos/92904/pexels-photo-92904.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
         class="w-full rounded"
-      >
+      />
     </div>
     <div class="px-2 pb-5 text-center">
-      <h1 class="text-4xl text-gray-800 text-thin mt-3 font-thin">Cameron Reece LaVallie</h1>
+      <h1 class="text-4xl text-gray-800 text-thin mt-3 font-thin">
+        Cameron Reece LaVallie
+      </h1>
       <p class="text-2xl text-gray-800 text-thin mb-3 font-thin">
         I built this site with
         <span class="text-blue-500 font-semibold">Nuxt | VueJS</span>
       </p>
       <div class="flex flex-wrap justify-center items-center">
-        <img class="w-48 m-1 rounded-full md:mr-4 lg:mr-4" src="~assets/images/NewCam.jpg">
+        <img
+          class="w-48 m-1 rounded-full md:mr-4 lg:mr-4"
+          src="~assets/images/NewCam.jpg"
+        />
         <div class="text-sm mt-3 md:mt-0 lg:mt-0">
           <p class="text-gray-900 leading-none text-xl font-thin">
             Junior Developer ~
@@ -21,27 +26,44 @@
           </p>
           <p class="text-gray-600 mt-3 font-thin">
             <span class="pr-1">
-              <a class="text-underline" target="blank" href="https://reactjs.org/">#ReactJS</a>
+              <a
+                class="text-underline"
+                target="blank"
+                href="https://reactjs.org/"
+                >#ReactJS</a
+              >
             </span>
             <span class="pr-1">
-              <a class="text-underline" target="blank" href="https://angularjs.org/">#AngularJS</a>
+              <a
+                class="text-underline"
+                target="blank"
+                href="https://angularjs.org/"
+                >#AngularJS</a
+              >
             </span>
             <span class="pr-1">
-              <a class="text-underline" target="blank" href="https://vuejs.org/">#VueJS</a>
+              <a class="text-underline" target="blank" href="https://vuejs.org/"
+                >#VueJS</a
+              >
             </span>
             <span class="pr-1">
-              <a class="text-underline" target="blank" href="https://nuxtjs.org/">#NuxtJS</a>
+              <a
+                class="text-underline"
+                target="blank"
+                href="https://nuxtjs.org/"
+                >#NuxtJS</a
+              >
             </span>
           </p>
         </div>
       </div>
     </div>
-    <hr>
+    <hr />
     <div class="flex flex-wrap justify-center text-center font-thin">
       <div class="m-12">
         <p>
           Bachelors
-          <br>
+          <br />
           <span class="text-blue-500 italic font-bold">(Graphic Design)</span>
         </p>
         <div class>
@@ -50,7 +72,7 @@
               class="w-48 rounded-lg origin-center transform hover:scale-75"
               src="~/assets/images/Evangel-logo.png"
               alt
-            >
+            />
           </a>
           <p class="underline">Evangel University Grad!</p>
         </div>
@@ -58,8 +80,10 @@
       <div class="m-12">
         <p class>
           Certified
-          <br>
-          <span class="text-blue-500 italic font-bold">(Software Developer)</span>
+          <br />
+          <span class="text-blue-500 italic font-bold"
+            >(Software Developer)</span
+          >
         </p>
         <div class>
           <a href="https://betheltech.net/" target="_blank" class>
@@ -67,7 +91,7 @@
               class="w-48 rounded-lg origin-center transform hover:scale-75"
               src="~/assets/images/bethel-tech-logo.jpg"
               alt
-            >
+            />
           </a>
           <p class="mt-2 underline">Bethel Tech Grad!</p>
         </div>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,24 +1,32 @@
-   <template>
-   <div class="container">
-
-<div class="text-3xl lg:text-5xl font-thin text-center mt-12">
+<template>
+  <div class="container">
+    <div class="text-3xl lg:text-5xl font-thin text-center mt-12">
       <p class="text-gray-900">Blog Coming Soon..</p>
       <p class="text-3xl text-gray-500">For now enjoy some beautiful photos!</p>
-    </div> 
-    <b-carousel :autoplay="false" indicator-custom :indicator-inside="false" :overlay="gallery">
-        <b-carousel-item v-for="(item, i) in 20" :key="i">
-            <a @click="switchGallery(true)" class="image ">
-                <img :src="getImgUrl(i)">
-            </a>
-        </b-carousel-item>
-        <span v-if="gallery" @click="switchGallery(false)" class="modal-close is-large"/>
-        <template slot="indicators" slot-scope="props">
-            <figure class="al image" :draggable="false">
-                <img :draggable="false" :src="getImgUrl(props.i)" :title="props.i">
-            </figure>
-        </template>
-    </b-carousel>
     </div>
+    <b-carousel
+      :autoplay="false"
+      indicator-custom
+      :indicator-inside="false"
+      :overlay="gallery"
+    >
+      <b-carousel-item v-for="(item, i) in 20" :key="i">
+        <a @click="switchGallery(true)" class="image ">
+          <img :src="getImgUrl(i)" />
+        </a>
+      </b-carousel-item>
+      <span
+        v-if="gallery"
+        @click="switchGallery(false)"
+        class="modal-close is-large"
+      />
+      <template slot="indicators" slot-scope="props">
+        <figure class="al image" :draggable="false">
+          <img :draggable="false" :src="getImgUrl(props.i)" :title="props.i" />
+        </figure>
+      </template>
+    </b-carousel>
+  </div>
 </template>
 
 <script>
@@ -28,35 +36,35 @@ import 'buefy/dist/buefy.css'
 
 Vue.use(Buefy)
 export default {
-    data() {
-        return {
-            gallery: false
-        }
-    },
-    methods: {
-        getImgUrl(value) {
-            value += 50
-            return `https://picsum.photos/id/10${value}/1230/500`
-        },
-        switchGallery(value) {
-            this.gallery = value
-            if (value) {
-                return document.documentElement.classList.add('is-clipped')
-            } else {
-                return document.documentElement.classList.remove('is-clipped')
-            }
-        }
+  data() {
+    return {
+      gallery: false
     }
+  },
+  methods: {
+    getImgUrl(value) {
+      value += 50
+      return `https://picsum.photos/id/10${value}/1230/500`
+    },
+    switchGallery(value) {
+      this.gallery = value
+      if (value) {
+        return document.documentElement.classList.add('is-clipped')
+      } else {
+        return document.documentElement.classList.remove('is-clipped')
+      }
+    }
+  }
 }
 </script>
 
 <style>
 .is-active .al img {
-    border: 1px solid #fff;
-    filter: grayscale(0%);
+  border: 1px solid #fff;
+  filter: grayscale(0%);
 }
 .al img {
-    border: 1px solid transparent;
-    filter: grayscale(100%);
+  border: 1px solid transparent;
+  filter: grayscale(100%);
 }
 </style>
